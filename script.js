9809955455291006f114cebf355498bc3210745d
VulpesPaw <richard.jh.johansson@gmail.com>
2022-09-05 17:16:09 +0200

    Stylising improvments, data handlers, raw data

--- script.js
+++ script.js
@@ -9,52 +9,76 @@
  * Todo: Get hold of old projects
  * Todo: Start linking
  * Todo: Put all of JS inside an anonym function
+ * Todo: add cliclible link for clarification
+ * Todo: do some fancy curls with heading, custom font or something
+ * Todo: Maek heading FEEL LIKE A FANCY HEADING BIG AND LARGE SEXY HEADOIGMdddddddd
+ * Todo: Intoruce an ALERT("") with a warning that due to termination of back end API's some projects may no longer work, check for screenshots and include in desc
+ * Todo: Include Firebase Thesis
+ * Todo: On down arrow decs. Make first text merge with down text, it otherwise douplets first row
  */
 
-//
 
-
-/* 
-
-
-*/
-
+// Todo: Continue writing desc for Firebase, check firebase webpage to see if it's still up and futher information
+// 
 
 async function getJSON() {
-  var resp = await fetch("./paths.json");
-  // todo remove log
-  console.log("json status:", resp.status);
-  return resp.json();
+  try {
+    var resp = await fetch("./paths.json");
+    // todo remove log
+    console.log("json status:", resp.status);
+    return resp.json();
+  } catch (error) {
+    console.error(error);
+    return;
+  }
 }
 
 getJSON().then((data) => {
   // JSON strucutre: JSON=>PATH{ARR[OBJ{intel,...}]}
-
+  console.log(data)
   let render = data.path
     .map(
       (obj) =>
         `<div id="${obj._id}" class="r_container">
-          <a href="${obj.url}">
-            <h3 class="r_title">${obj.title}</h3>
-            <h6 style="text-align: right">${obj.age}</h6>
-            <br />
-            <p><b>${obj.course}</b></p>
-            <p class="r_type">Stack type:<i>${obj.type}</i></p>
-            <p class="r_tag">Tags: <i>${obj.tag}</i></p>
+        <h4 style="text-align: right">${obj.age}</h4>
+            <a href="${obj.url}">
+            <h1 class="r_title">${obj.title}</h1>
+            
+            <p class="r_course"><b>${obj.course}</b></p>
+            <p class="r_type">Type:<i>${obj.type}</i></p>
+            <p class="r_tag">Tags: <i>${obj.tags.map(t=>t).join(", ")}</i></p><br>
             <img class="r_preview" src="${obj.img}" alt="Preview of _id:${
           obj.id
-        }"></a>
-            <details>
+        }"></a><br>
+        <h3 class="sub_title">${obj.sub_text}</h3>
+            <details id="dtl_${obj._id}">
+            
               <summary>${obj.info.slice(0, 25)}...</summary>
               <div class="content">${obj.info}</div>
             </details>
         </div>
+        
         `
     )
     .join("");
-
-  console.log("-- log 9 --", render);
-
-  // If json data was created from an uncontrolled source (eg. users), you should generate elements in a secure manner instead of applaying directly to inneHTML due to the possible use of JS.
   document.querySelector("#links").innerHTML = render;
+
+  /*
+    Note: If json data was created from un uncredited source (eg. users), you should not use .innerHTML as this allows javascript to excecute, and thus mallicous scripture may be deployed. In this case, the JSON comes from a trusted source.
+    */
 });
+/*
+,
+    {
+      "_id": "",
+      "title": "",
+      "age": "",
+      "url": "",
+      "course": "",
+      "tags": ["" ],
+      "type": "",
+      "sub_text": "",
+      "info": "",
+      "img": "./images/"
+    }
+*/
